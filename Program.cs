15843b5a6403ccc4a4bf19f400330ee37170a13b
user <user@WIN-793QUSRELGD>
2024-03-10 22:14:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace mas_0101_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double[] A = new double[10];
+                Console.WriteLine("Введите число: ");
+                double num = Convert.ToDouble(Console.ReadLine());
+                if (num > 0)
+                {
+                    A[0] = num;
+                }
+                else
+                { 
+                    throw new Exception("Недопустимый аргумент");
+                }
+                Console.Write($"Массив A: {A[0]} ");
+                for (int i = 1; i < 10; i++)
+                {
+                    A[i] = A[i / 2] + A[i - 1];
+                    Console.Write($"{A[i]} ");
+                }
+            }
+            catch (FormatException) 
+            {
+                Console.WriteLine("Неверный формат");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message); 
+            }
+        }
+    }
+}
---
+++ mas_0101_2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            int n = 3;
+            int[,] a = new int[n, n];
+            int[] mas1 = new int[6];
+            int[] mas2 = new int[3];
+            Random rand = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = rand.Next(3, 10);
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Массив элементов, лежащих на главной диагонали и выше: ");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if ((j > i) || (i == j))
+                    {
+                        mas1[i] = a[i, j];
+                        Console.Write(mas1[i] + " ");
+                    }
+
+                }
+            }
+            Console.WriteLine("\nМассив элементов, расположенных ниже главной диагонали: ");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i > j)
+                    {
+                        mas2[i] = a[i, j];
+                        Console.Write(mas2[i] + " ");
+
+                    }
+
+                }
+            }
+            Console.Write("\n");
+        }
+        catch(Exception) { }
+    }
+}
